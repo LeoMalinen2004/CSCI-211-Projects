f9654ab91f649a58b15a66bd4574b18c0a74f90a
Leo Malinen <107366630+LeoMalinen2004@users.noreply.github.com>
2023-12-21 15:53:32 -0800

    Create project2video.h

---
+++ project2video.h
@@ -0,0 +1,36 @@
+// video.h
+// Malinen, Leo
+// ldmalinen
+
+#ifndef VIDEO_h
+#define VIDEO_h
+#include <string>
+using namespace std;
+
+// class Video and all variables with functions below
+class Video {
+
+  // public functions and constructor declaration
+public:
+  Video(string title, string url, string comment, double length, int rating);
+  void insertStrings(string title, string url, string comment, double length,
+                     int rating);
+  void print();
+  void rating_sorter();
+  void title_sorter();
+  void length_sorter();
+  string titles();
+  double lengths();
+  int rating();
+  bool compare(Video *otherVideo);
+  // private variables
+private:
+  string m_title;
+  string m_url;
+  string m_comment;
+  double m_length;
+  int m_rating;
+  string m_asterisks;
+};
+#endif
+int Video::rating() { return m_rating; }
